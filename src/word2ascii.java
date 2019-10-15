81d6b58fab8f0f0a475375ec598d08b2fefa8a17
logperks <logperks@rams.colostate.edu>
2019-10-15 00:45:00 -0600

    Added more letter functionality and added safety feature.
    Added functionality for letters 'G' through 'N'.
    
    As I intend for this program to work with one character letters when
    creating the ASCII, I created an infinite while loop to make sure the
    length of the letter the user gives to make the ASCII is equal to one.

--- src/word2ascii.java
+++ src/word2ascii.java
@@ -8,8 +8,9 @@ public class word2ascii {
 	//XXXXX
 	
 	public static String transferLine(String word, String letter) {
-		String blank = " "; //String blank = ":black_large_square: ";
-		//letter = letter + " ";
+		String blank = " ";
+		
+//		letter should be single character
 		for (int i = 0; i < word.length(); i++) {
 			switch (Character.toUpperCase(word.charAt(i))){
 				case 'A':				
@@ -61,11 +62,12 @@ public class word2ascii {
 					System.out.println();
 					break;
 				case 'G':
-					System.out.println(blank + letter + letter + letter + letter);
-					System.out.println(letter + blank + blank + blank + letter);
+					System.out.println(letter + letter + letter + letter + letter);
+					System.out.println(letter + blank + blank + blank + blank);
 					System.out.println(letter + blank + letter + letter + letter);
 					System.out.println(letter + blank + blank + blank + letter);
 					System.out.println(letter + letter + letter + letter + letter);
+					System.out.println();
 					break;
 				case 'H':
 					System.out.println(letter + blank + blank + blank + letter);
@@ -73,8 +75,56 @@ public class word2ascii {
 					System.out.println(letter + letter + letter + letter + letter);
 					System.out.println(letter + blank + blank + blank + letter);
 					System.out.println(letter + blank + blank + blank + letter);
+					System.out.println();
 					break;
-					
+				case 'I':
+					System.out.println(letter + letter + letter + letter + letter);
+					System.out.println(blank + blank + letter + blank + blank);
+					System.out.println(blank + blank + letter + blank + blank);
+					System.out.println(blank + blank + letter + blank + blank);
+					System.out.println(letter + letter + letter + letter + letter);
+					System.out.println();
+					break;
+				case 'J':
+					System.out.println(letter + letter + letter + letter + letter);
+					System.out.println(blank + blank + blank + letter + blank);
+					System.out.println(blank + blank + blank + letter + blank);
+					System.out.println(letter + blank + blank + letter + blank);
+					System.out.println(letter + letter + letter + letter + letter);
+					System.out.println();
+					break;
+				case 'K':
+					System.out.println(letter + blank + blank + letter + blank);
+					System.out.println(letter + blank + letter + blank + blank);
+					System.out.println(letter + letter + blank + blank + blank);
+					System.out.println(letter + blank + letter + blank + blank);
+					System.out.println(letter + blank + blank + letter + blank);
+					System.out.println();
+					break;
+				case 'L':
+					System.out.println(letter + blank + blank + blank + blank);
+					System.out.println(letter + blank + blank + blank + blank);
+					System.out.println(letter + blank + blank + blank + blank);
+					System.out.println(letter + blank + blank + blank + blank);
+					System.out.println(letter + letter + letter + letter + letter);
+					System.out.println();
+					break;
+				case 'M':
+					System.out.println(blank + letter + blank + letter + blank);
+					System.out.println(letter + blank + letter + blank + letter);
+					System.out.println(letter + blank + letter + blank + letter);
+					System.out.println(letter + blank + letter + blank + letter);
+					System.out.println(letter + blank + letter + blank + letter);
+					System.out.println();
+					break;
+				case 'N':
+					System.out.println(letter + blank + blank + blank + letter);
+					System.out.println(letter + letter + blank + blank + letter);
+					System.out.println(letter + blank + letter + blank + letter);
+					System.out.println(letter + blank + blank + letter + letter);
+					System.out.println(letter + blank + blank + blank + letter);
+					System.out.println();
+					break;
 			}	
 		}
 		return "";
@@ -85,9 +135,18 @@ public class word2ascii {
 		
 		System.out.print("Input word to transfer: ");
 		String word = inputReader.next();
+		String letter = "";
 		
 		System.out.print("Input letter/emoji to make word out of: ");
-		String letter = inputReader.next();
+		
+		while (letter.length() != 1) {
+			letter = inputReader.next();
+			if (letter.length() != 1) {
+				System.out.println("Letter length must be equal to one.");
+				System.out.print("Input letter/emoji to make word out of: ");
+			}
+		}
+		
 		inputReader.close();
 		
 		transferLine(word, letter);
