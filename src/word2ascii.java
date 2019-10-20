90f0e89119da876f7c3fc9b72ea1cd5fdf8792da
logperks <logperks@rams.colostate.edu>
2019-10-19 18:32:10 -0600

    Modified final letter creation of "E" and "F".

--- src/word2ascii.java
+++ src/word2ascii.java
@@ -8,7 +8,7 @@ public class word2ascii {
 	//XXXXX
 	
 	public static String transferLine(String word, String letter) {
-		String blank = " ";
+		String blank = ":black_large_square:";
 
 		for (int i = 0; i < word.length(); i++) {
 			switch (Character.toUpperCase(word.charAt(i))){
@@ -30,9 +30,9 @@ public class word2ascii {
 					break;
 				case 'C':
 					System.out.println(blank + letter + letter + letter + letter);
-					System.out.println(letter);
-					System.out.println(letter);
-					System.out.println(letter);
+					System.out.println(letter + blank + blank + blank + blank);
+					System.out.println(letter + blank + blank + blank + blank);
+					System.out.println(letter + blank + blank + blank + blank);
 					System.out.println(blank + letter + letter + letter + letter);
 					System.out.println();
 					break;
@@ -46,18 +46,18 @@ public class word2ascii {
 					break;
 				case 'E':
 					System.out.println(letter + letter + letter + letter + letter);
-					System.out.println(letter);
-					System.out.println(letter + letter + letter);
-					System.out.println(letter);
+					System.out.println(letter + blank + blank + blank + blank);
+					System.out.println(letter + letter + letter + blank + blank);
+					System.out.println(letter + blank + blank + blank + blank);
 					System.out.println(letter + letter + letter + letter + letter);
 					System.out.println();
 					break;
 				case 'F':
 					System.out.println(letter + letter + letter + letter + letter);
-					System.out.println(letter);
-					System.out.println(letter + letter + letter);
-					System.out.println(letter);
-					System.out.println(letter);
+					System.out.println(letter + blank + blank + blank + blank);
+					System.out.println(letter + letter + letter + blank + blank);
+					System.out.println(letter + blank + blank + blank + blank);
+					System.out.println(letter + blank + blank + blank + blank);
 					System.out.println();
 					break;
 				case 'G':
@@ -86,9 +86,9 @@ public class word2ascii {
 					break;
 				case 'J':
 					System.out.println(letter + letter + letter + letter + letter);
-					System.out.println(blank + blank + blank + letter + blank);
-					System.out.println(blank + blank + blank + letter + blank);
-					System.out.println(letter + blank + blank + letter + blank);
+					System.out.println(blank + blank + blank + blank + letter);
+					System.out.println(blank + blank + blank + blank + letter);
+					System.out.println(letter + blank + blank + blank + letter);
 					System.out.println(letter + letter + letter + letter + letter);
 					System.out.println();
 					break;
@@ -235,13 +235,14 @@ public class word2ascii {
 			String word = inputReader.next();
 			String letter = "";
 
-			System.out.print("Input letter/emoji to make word out of: ");
+			System.out.print("Input letter to make word out of: ");
 
+			letter = inputReader.next();
 			while (letter.length() != 1) {
 				letter = inputReader.next();
 				if (letter.length() != 1) {
 					System.out.println("Letter length must be equal to one.");
-					System.out.print("Input letter/emoji to make word out of: ");
+					System.out.print("Input letter to make word out of: ");
 				}
 			}
 
@@ -262,6 +263,8 @@ public class word2ascii {
 				}
 			}
 		}
+		
 		inputReader.close();
+		
 	}
 }
